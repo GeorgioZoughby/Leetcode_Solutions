8d4c3533562fce6b514a34993df78748bd8bcccc
Georgio Zoughby <georgio.zoghby@outlook.com>
2024-08-13 14:15:33 +0300

    Solved

---
+++ 13_roman_to_integer.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+		static std::unordered_map<char, int> romanMap = {
+			{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+			{'C', 100}, {'D', 500}, {'M', 1000}
+		};
+
+		int result = 0;
+		for (int i = 0; i < s.size(); ++i) {
+			if (romanMap[s[i]] < romanMap[s[i + 1]])
+			{
+				result -= romanMap[s[i]];
+			}
+			else
+			{
+				result += romanMap[s[i]];
+			}
+		}
+		return result;
+    }
+};
