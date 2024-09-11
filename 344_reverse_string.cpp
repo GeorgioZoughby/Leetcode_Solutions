1ecdcc3523775c77500907a3914fead5155e9760
Georgio Zoughby <georgio.zoghby@outlook.com>
2024-09-11 16:53:37 +0300

    Solved using two pointer approach

---
+++ 344_reverse_string.cpp
@@ -0,0 +1,15 @@
+// Author: Georgio Zoughby
+
+#include <vector>
+
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right){
+            std::swap(s[left], s[right]);
+            ++left;
+            --right;
+        }
+    }
+};
